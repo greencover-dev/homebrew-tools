49a00afb7c1165bcc1fcfe6ceceaec74dae3d665
marques-work <723642+marques-work@users.noreply.github.com>
2022-10-03 19:30:19 +0000

    licensure: add 0.2.1 bottle.

--- Formula/licensure.rb
+++ Formula/licensure.rb
@@ -9,6 +9,11 @@ class Licensure < Formula
     sha256 "6b070a217643f047fae5cde3fbcdde39afe92e31640d58b66cb7388ac190391d"
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/greencover-dev/tools"
+    sha256 cellar: :any_skip_relocation, monterey: "d5e9273401ae0e90d348c231fa48a6f57ff485fd6cf4afcc99af1947454166e9"
+  end
+
   depends_on "rust" => :build
 
   def install
